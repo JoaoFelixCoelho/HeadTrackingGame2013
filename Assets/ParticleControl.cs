8732f8cbb82f4abbddf0466ed7a0b4fa40f06d0e
unknown <39097882@B-TICL3-08.belgrano.ort.arg>
2013-08-21 10:05:51 -0300

    Nuevo Material, particulas 1.1

---
+++ Assets/ParticleControl.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+
+public class ParticleControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//particleSystem.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
