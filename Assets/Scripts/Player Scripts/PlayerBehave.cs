a5bdd208c43aff5e6a8db8fa9606c431c41fc5a4
unknown <ingles@B-TICL3-04.belgrano.ort.arg>
2013-10-02 12:16:14 -0300

    Animacion hecha + YAPA

--- Assets/Scripts/Player Scripts/PlayerBehave.cs
+++ Assets/Scripts/Player Scripts/PlayerBehave.cs
@@ -4,7 +4,9 @@ using System.Collections;
 public class PlayerBehave : MonoBehaviour {
 
 	public int ammo;
-	public Arma weapon;
+	public GameObject rifle;
+	public GameObject laser;
+	private Arma weapon;
 	public int kills;
 	public int killsInRound;
 	public int score;
@@ -23,6 +25,7 @@ public class PlayerBehave : MonoBehaviour {
 	void Start () {
 		this.kills=0;
 		this.score=0;
+		rifle.gameObject.SetActive(true);
 		//this.currHp = totalHp;
 	}
 	
@@ -32,32 +35,10 @@ public class PlayerBehave : MonoBehaviour {
 		}
 	}
 	
-	/*public void damageHp(int cant){
-		if (this.currHp>0 && this.currHp-cant>0){
-			this.currHp-=cant;
-		}
-			else {
-				killPlayer();	
-			}
-		}*/
-
 	public void reloadWep(){
 		// .reload te devuelve la cantidad de balas que van a quedar, le llegan la cantidad que tenés
 		this.ammo-=(weapon.reload(this.ammo));
 	}
-	
-	
-	
-	/*public int addHp (int cant){
-		if (this.totalHp>currHp+cant){
-			this.currHp+=cant;
-		}
-		else {
-			this.currHp=this.totalHp;
-		}
-		return currHp;//devuelvo la vida que queda, seguro lo termino usando o algo
-	}
-	 */
 	
 	public void killPlayer () {
 		Debug.Log("El jugador se murio guacho");
@@ -74,47 +55,9 @@ public class PlayerBehave : MonoBehaviour {
 	void Update () {
 		bool isLeft = Input.GetKey(KeyCode.Mouse0);
 		bool rKey = Input.GetKeyDown(KeyCode.R);
+		bool gKey = Input.GetKeyDown(KeyCode.G);
 		if(isLeft) {
-			//print (GetComponent<Arma>().shooting);
 			weapon.shoot();	
 		}
-		
-	/*	//Debug.Log (fireRate);
-		bool isLeft = Input.GetKey(KeyCode.Mouse0);
-	//	bool isA = wiimote_getButtonA(0);
-//		bool isB = wiimote_getButtonB(0);
-		bool rKey = Input.GetKeyDown(KeyCode.R);
-		bool zKey = Input.GetKeyDown(KeyCode.Z);
-		bool xKey = Input.GetKeyDown(KeyCode.X);
-		bool cKey = Input.GetKeyDown(KeyCode.C);
-		fireRate -= Time.deltaTime;
-		if (fireRate < 0){
-			fireRate = 0.2;
-			allowFire=true;
-		}
-			else 
-				allowFire=false;
-			
-		if (rKey || isA){
-			this.reloadWep();
-			print("municion que queda: " + this.ammo);
-		}
-	
-				
-		if (isLeft && resetBool==true){
-			fireRate = 0;
-			resetBool=false;
-		}
-		if (isLeft && allowFire==true || isB && allowFire==true) {
-			weapon.shoot();	
-		}
-		if (isLeft=false){
-			resetBool=true;
-		}
-		*/
-
-		
 	}
-	
-	
 }
