bcfd1a406941bc9689e1b1591ebb0b9d39b170e4
unknown <38788439@B-TICL3-07.belgrano.ort.arg>
2013-10-16 12:19:03 -0300

    base de datos

--- Assets/Scripts/Misc/Score.cs
+++ Assets/Scripts/Misc/Score.cs
@@ -1,13 +1,13 @@
 using UnityEngine;
-//using MySql.Data;
-//using MySql.Data.MySqlClient;
+using MySql.Data;
+using MySql.Data.MySqlClient;
 using System;
-//using System.Data;
+using System.Data;
 using System.Collections;
 using System.Collections.Generic;
 
 public class Score : MonoBehaviour {
-/*	
+	
 	public string serverIP = "127.0.0.1";
     private string database = "unity";
     private string UserDataBase = "root";
@@ -40,6 +40,29 @@ public class Score : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
-	}*/
+	}
+	
+	
+	
+	// Insert new entries into the table
+	void Insert()
+	{
+	prepData();
+	string query = string.Empty;
+	query = "INSERT INTO cuentas (numbre, puntaje) VALUES (?nombre, ?puntaje)";
+	if (con.State != ConnectionState.Open)
+	con.Open();
+	using (con){
+		foreach (data itm in _GameItems){
+			using (cmd = new MySqlCommand(query, con)){
+				MySqlParameter oParam = cmd.Parameters.Add("?ID", MySqlDbType.VarChar);
+				oParam.Value = itm.ID;
+				MySqlParameter oParam1 = cmd.Parameters.Add("?Name", MySqlDbType.VarChar);
+				oParam1.Value = itm.Name;
+			}
+		}
+	}
+	
+	
+	
 }
-       
