861a9cdf9f6878b470b454273790ef2aa2e8fced
unknown <39245372@B-TICL3-07.belgrano.ort.arg>
2013-10-23 11:22:59 -0300

    Arreglados erroeres del teclado, borra y espacia(?)

--- Assets/Scripts/Misc/KeyboardScript.cs
+++ Assets/Scripts/Misc/KeyboardScript.cs
@@ -4,14 +4,29 @@ using System.Collections;
 public class KeyboardScript : MonoBehaviour {
 	private string letter;
 	public string name;
+	public TextMesh textoMonitor;
+
 	void press(){
-	if (Input.anyKey){
-		letter = Input.inputString;
-		transform.FindChild(letter).animation.Play(letter+"Key");
-		name += letter;
-		print (name);
+		bool isBackSpace = Input.GetKeyDown(KeyCode.Backspace);
+		bool isSpace = Input.GetKeyDown(KeyCode.Space);
+		if (isBackSpace && name.Length-1>=0){
+				 name = name.Substring(0, name.Length - 1);
+			transform.FindChild("backspace").animation.Play("eraseKey");
+			}
+		if (isSpace){
+				 name += " ";
+			transform.FindChild("space").animation.Play("spaceKey");
 		}
-		
+		if (Input.anyKey){
+		letter = Input.inputString;
+			if (transform.FindChild(letter)!=null) {
+				transform.FindChild(letter).animation.Play(letter+"Key");
+				name += letter;
+				print(Input.inputString);
+			}
+			
+		}
+		textoMonitor.text = name;
 	}
 	// Use this for initialization
 	void Start () {
