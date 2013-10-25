a0a052ff04294861106322b60cf981c26c6568a6
Ignacio Cozza <ignaciocozza@gmail.com>
2013-10-24 22:38:03 -0300

    Arreglado tiling, upgrade materiales, IntroScene mas fachera, audio en teclado

--- Assets/Scripts/Misc/KeyboardScript.cs
+++ Assets/Scripts/Misc/KeyboardScript.cs
@@ -11,28 +11,30 @@ public class KeyboardScript : MonoBehaviour {
 		bool isSpace = Input.GetKeyDown(KeyCode.Space);
 		bool isReturn = Input.GetKeyDown(KeyCode.Return);
 		
-		if (isBackSpace && name.Length-1>=0){
-				 name = name.Substring(0, name.Length - 1);
-			transform.FindChild("backspace").animation.Play("eraseKey");
-			}
-		
-		if (isSpace){
-				 name += " ";
-			transform.FindChild("space").animation.Play("spaceKey");
-		}
-		
-		if (isReturn){
-			transform.FindChild("enter").animation.Play("enterKey");
-		}
-		
 		if (Input.anyKey){
-		letter = Input.inputString;
+			audio.Play();
+			
+			if (isBackSpace && name.Length-1>=0){
+					 name = name.Substring(0, name.Length - 1);
+				transform.FindChild("backspace").animation.Play("eraseKey");
+			}
+			
+			if (isSpace){
+					 name += " ";
+				transform.FindChild("space").animation.Play("spaceKey");
+			}
+			
+			if (isReturn){
+				transform.FindChild("enter").animation.Play("enterKey");
+			}
+			
+			letter = Input.inputString;
 			if (transform.FindChild(letter)!=null && name.Length<15) {
 				transform.FindChild(letter).animation.Play(letter+"Key");
 				name += letter;
-			}	
+			}
+			textoMonitor.text = name;
 		}
-		textoMonitor.text = name;
 	}
 	// Use this for initialization
 	void Start () {
