297ac73b7ad2e1a6061a67240619aa3ebbfdd457
unknown <38788439@B-TICL3-09.belgrano.ort.arg>
2013-10-29 15:59:06 -0300

    score

--- Assets/Scripts/Misc/Score.cs
+++ Assets/Scripts/Misc/Score.cs
@@ -12,57 +12,53 @@ public class Score : MonoBehaviour {
     private string database = "unity";
     private string UserDataBase = "root";
     private string PassDataBase = "root";
+	private MySqlConnection cn;
 	
 	string user, puntaje;
 	
-	public class MySQLCS : MonoBehaviour {
-		
-
+	public struct data
+	{
+		public int puntaje;
+		public string nombre;
 	}
 	
-	/*void score() {
-		
-        MySqlConnection cn = new MySqlConnection("Server=" + serverIP + ";Database=" + database + ";User ID=" + UserDataBase + ";Password=" + PassDataBase + ";Pooling=false");
+	public void con() {
+        cn = new MySqlConnection("Server=" + serverIP + ";Database=" + database + ";User ID=" + UserDataBase + ";Password=" + PassDataBase + ";Pooling=false");
         cn.Open();
         Debug.Log("Conexion con la base de datos correcta");
-        MySqlCommand cmd = new MySqlCommand("SELECT * FROM cuentas WHERE user = '" + user + "';");
+        MySqlCommand cmd = new MySqlCommand("SELECT * FROM cuentas WHERE nombre = '" + user + "';", cn);
         cmd.Connection = cn;
         MySqlDataReader reader = cmd.ExecuteReader();
-		
 
 	}
 	
 	// Use this for initialization
 	void Start () {
-		score();
-	}
+		con();
+	} 
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
 	
-	
-	
 	// Insert new entries into the table
-	void Insert()
-	{
-	prepData();
-	string query = string.Empty;
-	query = "INSERT INTO cuentas (numbre, puntaje) VALUES (?nombre, ?puntaje)";
-	if (con.State != ConnectionState.Open)
-	con.Open();
-	using (con){
-		foreach (data itm in _GameItems){
-			using (cmd = new MySqlCommand(query, con)){
-				MySqlParameter oParam = cmd.Parameters.Add("?ID", MySqlDbType.VarChar);
-				oParam.Value = itm.ID;
-				MySqlParameter oParam1 = cmd.Parameters.Add("?Name", MySqlDbType.VarChar);
-				oParam1.Value = itm.Name;
-			}
-		}
-	}
-*/	
-	
+	void Insert(){
+		int puntaje = PlayerBehave.score;
+		string nombre = string.Empty;		
+		string query = string.Empty;
+		
+		query = "INSERT INTO cuentas (nombre, puntaje) VALUES (?nombre, ?puntaje)";
+		if (cn.State != ConnectionState.Open)
+			cn.Open();
+		MySqlCommand cmd = new MySqlCommand(query, cn);
+		MySqlParameter oParam = cmd.Parameters.Add("?nombre", MySqlDbType.VarChar);
+		oParam.Value = nombre;
+		MySqlParameter oParam1 = cmd.Parameters.Add("?puntaje", MySqlDbType.Int32);
+		oParam1.Value = puntaje;
+		
+		cmd.ExecuteNonQuery();
+		
+	}	
 	
 }
