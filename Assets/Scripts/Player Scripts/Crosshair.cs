46f0ba716c4adac4132dc5152ed15f2f9b33d8f6
Luffy <ale.radisic@gmail.com>
2013-11-13 02:29:24 -0200

    Menu de pausa resizeable y casi fully functional + YAPA (Mentira esta vez no hay yapa :/)

--- Assets/Scripts/Player Scripts/Crosshair.cs
+++ Assets/Scripts/Player Scripts/Crosshair.cs
@@ -18,54 +18,22 @@ public class Crosshair : MonoBehaviour {
 	private bool aversh;
 	
 	
-
-	// Use this for initialization
 	void Start () {
 		WiiMote.wiimote_start();
-		//mira = (Texture2D) Resources.Load("crosshair");
 	}
- 
-	// Update is called once per frame
-	void Update () {
-	
-/*		float roll = Mathf.Round(WiiMote.wiimote_getRoll(Configuration.pointerWiiMote));
-		float p = Mathf.Round(WiiMote.wiimote_getPitch(Configuration.pointerWiiMote));
-		float yaw = Mathf.Round(WiiMote.wiimote_getYaw(Configuration.pointerWiiMote));
-		float ir_x = WiiMote.wiimote_getIrX(Configuration.pointerWiiMote);
-		float ir_y = WiiMote.wiimote_getIrY(Configuration.pointerWiiMote);
-
-/*		Vector3 vec = new Vector3(p, 0 , -1 * roll);
-		vec = Vector3.Lerp(oldVec, vec, Time.deltaTime * 5);
-		oldVec = vec;
-
-		float temp_x = ( Screen.width / 2) + ir_x * (float) Screen.width / (float)2.0;
-		float temp_y = Screen.height - (ir_y * (float) Screen.height / (float)2.0);
-		mira_x = Mathf.RoundToInt(temp_x);
-		mira_y = Mathf.RoundToInt(temp_y);
-		if (mira_x > 0 && mira_y >0) {
-			return Camera.main.camera.ScreenToWorldPoint(new Vector3(mira_x + 37f, Screen.height * 1.5f - mira_y - 37f , 20));
-		}
-		else {
-				
-			
-		}
-	*/	    
-	}
+ 	
  
 		
 	public static Vector3 getWiimoteCrosshair() {
 		
 		float ir_x = WiiMote.wiimote_getIrX(Configuration.pointerWiiMote);
 		float ir_y = WiiMote.wiimote_getIrY(Configuration.pointerWiiMote);	
-		
+
 		ir_x = ( Screen.width / 2) + ir_x * (float) Screen.width / (float)2.0;
 		ir_y = Screen.height - (ir_y * (float) Screen.height / (float)2.0);
-		
-		if (ir_x > 0 && ir_y >0) {
-			
-			return Camera.main.camera.ScreenToWorldPoint(new Vector3(ir_x + 37f, Screen.height * 1.5f - ir_y - 37f , 20));
 
-			
+		if (ir_x > 0 && ir_y >0) {
+			return Camera.main.camera.ScreenToWorldPoint(new Vector3(ir_x + 37f, Screen.height * 1.5f - ir_y - 37f , 20));	
 		}
 		else {	
 			return new Vector3(1,1,1);
--- Assets/Scripts/Player Scripts/PlayerBehave.cs
+++ Assets/Scripts/Player Scripts/PlayerBehave.cs
@@ -21,6 +21,11 @@ public class PlayerBehave : MonoBehaviour {
 	public GUIText enemyCounter;
 	public GUIText warningGUI;
 	public GUIText roundWarning;
+	public GUIText resume;
+	public GUIText options;
+	public GUIText saveAndExit;
+	public GUIText exitToWindows;
+	public GUIText exitToMenu;
 	#endregion
 
 	
@@ -38,10 +43,17 @@ public class PlayerBehave : MonoBehaviour {
 	public GameObject pauseMenu;
 	private bool menuOpen;
 	private bool pauseCheck = false;
+	//public enum MenuEnum {Continue = 1, Options=2, SaveAndExit=3, ExitToMenu=4, ExitToWindows=5};
+	public int menuNavigator=1;
+	public Color lowOpacity;
+	public Color normalOpacity;
+	public bool canUp=false, canDown = false, letUp, letDown;
+	public float timerUpDown;
 	#endregion
 	
 	#region others
 	private bool handtolaser;
+
 	#endregion
 	
 	
@@ -55,6 +67,13 @@ public class PlayerBehave : MonoBehaviour {
 			weapon = laser.GetComponent<Arma>();
 		}
 		//this.currHp = totalHp;
+		lowOpacity = new Color (resume.font.material.color.r, resume.font.material.color.g, resume.font.material.color.b,0.1f);
+		normalOpacity = new Color (resume.font.material.color.r, resume.font.material.color.g, resume.font.material.color.b,1f);
+		resume.material.color = normalOpacity;
+		options.material.color = lowOpacity;
+		saveAndExit.material.color = lowOpacity;
+		exitToMenu.material.color = lowOpacity;
+		exitToWindows.material.color = lowOpacity;
 	}
 	
 	public void addAmmo(int cant){
@@ -181,7 +200,124 @@ public class PlayerBehave : MonoBehaviour {
 	}
 	
 	public void resizeGui(){
+		float spacing = Screen.height/10;
 		background.pixelInset = new Rect(-Screen.width/2, -Screen.height/2, Screen.width, Screen.height);
+		resume.pixelOffset = new Vector2(0,0+resume.fontSize);
+		options.pixelOffset = new Vector2(0,resume.pixelOffset.y-options.fontSize-spacing);
+		saveAndExit.pixelOffset = new Vector2 (0,options.pixelOffset.y-saveAndExit.fontSize-spacing);
+		exitToMenu.pixelOffset = new Vector2 (0,saveAndExit.pixelOffset.y-saveAndExit.fontSize-spacing);
+		exitToWindows.pixelOffset = new Vector2(0,exitToMenu.pixelOffset.y-saveAndExit.fontSize-spacing);
+		
+	}
+	
+	public void selectOption(){
+		bool isUp = Input.GetKeyDown(KeyCode.UpArrow);
+		bool isDown = Input.GetKeyDown(KeyCode.DownArrow);
+		bool isEnter = Input.GetKeyDown(KeyCode.Return);
+		bool isWiiDown = WiiMote.wiimote_getButtonDown(Configuration.pointerWiiMote);
+		bool isWiiUp = WiiMote.wiimote_getButtonUp(Configuration.pointerWiiMote);
+		
+		if (isWiiUp){
+			letUp= true;
+			letUp=false;
+			canUp = true;
+			if (canUp){
+				timerUpDown+=Time.deltaTime;
+				if (timerUpDown>0.5f){
+					letUp=true;
+					timerUpDown=0;
+					letUp=false;
+				}
+			}
+			else { 
+				canUp = false;
+			}
+		}
+		
+		if (isEnter){
+			switch (menuNavigator){
+			case 1:
+				pauseGame();
+			break;
+			case 3:
+				StartCoroutine(RoundManager.dbController.PostScores(PlayerBehave.playerName, PlayerBehave.score));
+				Application.LoadLevel(0);
+				break;
+			case 4:
+				Application.LoadLevel(0);
+			break;
+			case 5:
+				Application.Quit ();
+			break;
+			}
+		}
+		
+		//navegar con flechitas
+		if (isUp || letUp){
+			menuNavigator-=1;
+			switch (menuNavigator){
+				case 0:
+					menuNavigator=5;
+					resume.material.color = lowOpacity;
+					exitToWindows.material.color = normalOpacity;
+				break;
+				case 1:
+					resume.material.color = normalOpacity;
+					options.material.color = lowOpacity;
+					break;
+				case 2:
+					options.material.color = normalOpacity;
+					saveAndExit.material.color = lowOpacity;
+				break;
+				case 3:
+					exitToMenu.material.color = lowOpacity;
+					saveAndExit.material.color = normalOpacity;
+				break;
+				case 4:
+					exitToWindows.material.color = lowOpacity;
+					exitToMenu.material.color = normalOpacity;
+				break;
+				case 5:
+					resume.material.color = lowOpacity;
+					exitToWindows.material.color = normalOpacity;
+				break;
+			}	
+		}	
+		if (isDown){
+			menuNavigator+=1;
+				switch (menuNavigator){
+				case 1:
+					resume.material.color = normalOpacity;
+					exitToWindows.material.color = lowOpacity;
+					break;
+				case 2:
+					resume.material.color = lowOpacity;
+					options.material.color = normalOpacity;
+				break;
+				case 3:
+					options.material.color = lowOpacity;
+					saveAndExit.material.color = normalOpacity;
+				break;
+				case 4:
+					saveAndExit.material.color = lowOpacity;
+					exitToMenu.material.color = normalOpacity;
+				break;
+				case 5:
+					exitToMenu.material.color = lowOpacity;
+					exitToWindows.material.color = normalOpacity;
+				break;
+				case 6:
+				menuNavigator=1;
+				resume.material.color = normalOpacity;
+				exitToWindows.material.color = lowOpacity;
+				break;
+			}	
+		}
+		//navegar con flechitas
+	}
+	
+	public void wiiMenu(){
+		
 	}
 	
 	void Update () {
@@ -200,7 +336,10 @@ public class PlayerBehave : MonoBehaviour {
 			
 		
 		
-		
+		if (menuOpen){
+			selectOption();
+			
+		}
 		
 		if (imageEffectActive) {
 			checkCameraDistort();
